698b2b43f981ad63dae332ab02afb5d8f81ca612
midhunskr <midhun.shankar.199@gmail.com>
2025-04-28 13:28:39 +0530

    fixed auth_error_second_time

--- app/api/friends/reject/[requestId]/route.js
+++ app/api/friends/reject/[requestId]/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 import prisma from '../../../../../lib/prisma'
-import { authOptions } from '../../../../../lib/auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 
 export async function POST(request, { params }) {
     try {
